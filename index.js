1491b17f35b64083854f151ee39639f2845eb4b1
Matthew Trueblood <matt@matthewtrueblood.com>
2017-08-15 16:18:58 -0400

    Added meganav

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.set('view engine', '.hbs');
 app.set('views', 'views');
 
 let Storyblok = new StoryblokClient({
-  privateToken: 'a6yDcOOh8puW4Y8YIa7Q7Qtt'
+  privateToken: 'a6yDcOOh8puW4Y8YIa7Q7Qtt',
+  cache: {
+    type: 'memory'
+  }
 });
 
 // 3. Define a wilcard route to get the story mathing the url path
@@ -25,20 +28,38 @@ app.get('/*', function(req, res) {
   path = path == '/' ? 'home' : path; // this will allow to visit "home" from / only but still allow the editor to access the home "page" on localhost:4300/home
 
   Storyblok
-    .get(`stories/${path}`, { // dynamic variable also here!
+    .get(`stories/meganav`, { // load the global Story upfront so we can pass it in line 41
       version: req.query._storyblok ? 'draft': 'published'
     })
-    .then((response) => {
-      res.render(response.body.story.content.component, { // changed to dynamic "base" component -> Since you've created 2 components "home" and "about" in your components overview: http://app.storyblok.com/#!/me/spaces/40936/components/ those are also "root" components and therefor can define another layout! :)
-        story: response.body.story
-      });
-    })
+    .then((meganavResponse) => {
+      let meganavData = meganavResponse.body.story.content;
+
+      Storyblok
+        .get(`stories/${path}`, { // dynamic variable also here!
+          version: req.query._storyblok ? 'draft': 'published'
+        })
+
+        .then((response) => {
+          res.render(response.body.story.content.component, { // changed to dynamic "base" component -> Since you've created 2 components "home" and "about" in your components overview: http://app.storyblok.com/#!/me/spaces/40936/components/ those are also "root" components and therefor can define another layout! :)
+            story: response.body.story,
+            meganav: meganavData // pass the global content to Handlebars
+          });
+        })
+
+        .catch((error) => {
+          res.send(error);
+        });
+        
+      })
     .catch((error) => {
       res.send(error);
     });
 });
 
-
+app.get('clear_cache', function(req, res) {
+  Storyblok.flushCache();
+  res.send('Cache flushed!');
+});
 
 app.use('/public', express.static('public'));
 
