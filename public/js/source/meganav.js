cbc713e85be454060960fe07223bb73370e23e8f
Matthew Trueblood <matt@matthewtrueblood.com>
2017-09-19 23:14:08 -0400

    Added more sass and structure

--- public/js/source/meganav.js
+++ public/js/source/meganav.js
@@ -5,11 +5,27 @@
         $('.meganav-overlay').addClass('open');
       })
     });
+
     $('.meganav-overlay .toggle-meganav-overlay').click(function() {
       $('.meganav-overlay').removeClass('open');
       $('.meganav-strip .toggle-meganav-overlay').fadeIn(300);
     });
+
     var pageName = $("body").data("page");
     $(".meganav-overlay div a."+pageName).addClass("active");
+
+
+    $(window).scroll(function(){
+        var ScrollTop = parseInt($(window).scrollTop());
+        if (ScrollTop > 100) {
+            $(".meganav-strip").addClass("scrolled");
+          } else {            
+            $(".meganav-strip").removeClass("scrolled");
+        }
+    });
+
+
+
+
   });
 })(jQuery);
